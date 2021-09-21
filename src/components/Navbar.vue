6abc6996580e6868558dac478f90a0f30accd3f9
MelhorQueSVN <joaonunes.22.12@gmail.com>
2021-09-21 16:13:49 +0100

    navbar pratically done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav>
-        <v-toolbar dense flat transparent app> 
+        <v-toolbar dense flat transparent> 
             <v-container>
                 <v-row> 
                     <!-- 11 cols dedicadas para os 4 principais e 1 para a escolha da linguagem -->
@@ -15,7 +15,22 @@
                     </v-col>
                     
                     <v-col cols="1" justify="end"> 
-                        <v-btn text> Language </v-btn>
+                        
+                        <v-menu>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn text v-bind="attrs" v-on="on"> Language </v-btn>
+                            </template>
+                            <v-list>
+                                <v-list-item v-for="(language,index) in list_languages" :key="index">
+                                    <v-list-item-title>
+                                        <v-btn text @click="changeLanguage(language.title)">
+                                            {{language.title}}
+                                        </v-btn>
+                                    </v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                   
                     </v-col>
                 </v-row>
 
@@ -26,6 +41,19 @@
 
 <script>
 export default {
-
+    data() { 
+        return { 
+            list_languages : [
+                { title : 'English'}, 
+                { title : 'Português'}
+            ]
+        }
+    }, 
+    methods : { 
+        
+        changeLanguage(title) {
+            console.log("Change to: " + title)
+        }
+    }
 }
 </script>
