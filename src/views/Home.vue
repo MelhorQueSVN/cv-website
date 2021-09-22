d34231879e498245d20c0f592b6dba1014740b71
MelhorQueSVN <joaonunes.22.12@gmail.com>
2021-09-22 15:13:41 +0100

    sidecard done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -69,8 +69,9 @@
               <v-container class="text-center">
                   
                   <v-row> 
-
-                    <v-col cols="4" class="aboutMefirstColColor">
+                    
+                    <!-- v-col não tem xs, portanto com cols="12" faz fill do ecrã, depois devices com md para cima fica apenas com 4 -->
+                    <v-col cols="12" class="aboutMefirstColColor" md="4">
 
                       <v-avatar size="130">
                         <v-img
@@ -79,12 +80,275 @@
                         ></v-img>
                       </v-avatar>
 
-                      <h2 class="white--text"> João <span class="blue--text">Nunes</span> </h2>
-                      <h2 class="white--text"> An aspiring web developer </h2>
+                      <h2 class="aboutMeTextColor"> João <span class="blue--text">Nunes</span> </h2>
+                      <h2 class="aboutMeTextColor"> An aspiring web developer </h2>
 
+                      <h3 class="aboutMeTextColor text-left pt-6"> Info </h3>
+                      <v-divider> </v-divider>
+
+                      <!-- EMAIL -->
+                      <v-row class="pt-3" align="center">
+                        <!-- Temos uma row dividida numa coluna para o icon seguido de outra coluna dividida em 2 rows, em ecrãs pequenos e grandes 20% vai para o col icon -->
+                        <!-- e 80% é dedicada à col que contem os dados -->
+                        <v-col cols="2" md="2" class="text-left"> 
+                              <v-icon
+                                large
+                                color="white"
+                              >
+                                mdi-email
+                              </v-icon>
+                        </v-col> 
+                        
+                        <v-col cols="10" md="10" class="text-left aboutMeTextColor">
+                          <v-row align="center">
+                            Email
+                          </v-row> 
+
+                          <v-row align="center">
+                            joaonunes.22.12@gmail.com
+                          </v-row> 
+                        </v-col>                    
+                      </v-row>
+                   
+                      <!-- WEBSITE -->
+                      <v-row class="pt-3" align="center">
+                        <!-- Temos uma row dividida numa coluna para o icon seguido de outra coluna dividida em 2 rows, em ecrãs pequenos e grandes 20% vai para o col icon -->
+                        <!-- e 80% é dedicada à col que contem os dados -->
+                        <v-col cols="2" md="2" class="text-left"> 
+                              <v-icon
+                                large
+                                color="white"
+                              >
+                                mdi-search-web
+                              </v-icon>
+                        </v-col> 
+                        
+                        <v-col cols="10" md="10" class="text-left aboutMeTextColor">
+                          <v-row align="center">
+                            Website
+                          </v-row> 
+
+                          <v-row align="center">
+                            joao-nunes-cv.pt
+                          </v-row> 
+                        </v-col>                    
+                      </v-row>
+
+
+                      <!-- NASCIMENTO -->
+                      <v-row class="pt-3" align="center">
+                        <!-- Temos uma row dividida numa coluna para o icon seguido de outra coluna dividida em 2 rows, em ecrãs pequenos e grandes 20% vai para o col icon -->
+                        <!-- e 80% é dedicada à col que contem os dados -->
+                        <v-col cols="2" md="2" class="text-left"> 
+                              <v-icon
+                                large
+                                color="white"
+                              >
+                                mdi-cake-variant
+                              </v-icon>
+                        </v-col> 
+                        
+                        <v-col cols="10" md="10" class="text-left aboutMeTextColor">
+                          <v-row align="center">
+                            Birthday
+                          </v-row> 
+
+                          <v-row align="center">
+                            22 November 1998
+                          </v-row> 
+                        </v-col>                    
+                      </v-row>
+
+
+                      <!-- LOCALIZAÇÃO -->
+                      <v-row class="pt-3" align="center">
+                        <!-- Temos uma row dividida numa coluna para o icon seguido de outra coluna dividida em 2 rows, em ecrãs pequenos e grandes 20% vai para o col icon -->
+                        <!-- e 80% é dedicada à col que contem os dados -->
+                        <v-col cols="2" md="2" class="text-left"> 
+                              <v-icon
+                                large
+                                color="white"
+                              >
+                                mdi-map-marker
+                              </v-icon>
+                        </v-col> 
+                        
+                        <v-col cols="10" md="10" class="text-left aboutMeTextColor">
+                          <v-row align="center">
+                            Location
+                          </v-row> 
+
+                          <v-row align="center">
+                            Lousada, Portugal
+                          </v-row> 
+                        </v-col>                    
+                      </v-row>
+
+                      <h3 class="aboutMeTextColor text-left pt-6"> You can also find me on </h3>
+                      <v-divider> </v-divider>
+
+                      <v-row> 
+                        
+                        <v-col> 
+                          <v-btn icon color="white" href="https://github.com/MelhorQueSVN" target="_blank">  
+                            <v-icon> mdi-github</v-icon>
+                          </v-btn>
+                        </v-col>
+
+                        <v-col> 
+                          <v-btn icon color="white" href="https://pt.linkedin.com/" target="_blank">  
+                            <v-icon> mdi-linkedin</v-icon>
+                          </v-btn>
+                        </v-col>
+
+                        <v-col> 
+                          <v-btn icon color="white" href="https://www.facebook.com/joao.filipe.16940/" target="_blank">  
+                            <v-icon> mdi-facebook</v-icon>
+                          </v-btn>
+                        </v-col>
+
+                        <v-col> 
+                          <v-btn icon color="white" href="https://www.instagram.com/joaoflpnunes/" target="_blank">  
+                            <v-icon> mdi-instagram</v-icon>
+                          </v-btn>
+                        </v-col>
+
+                      </v-row>
+
+                      <h3 class="aboutMeTextColor text-left pt-3"> Things I enjoy in my free time </h3>
+                      <v-divider> </v-divider> 
+
+                      <!-- 1a fila de hobbies -->
+                      <v-row class="pt-3"> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-book-open-page-variant
+                            </v-icon>
+                             Reading
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-dumbbell
+                            </v-icon>
+                             Training
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-music
+                            </v-icon>
+                             Music
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-gamepad
+                            </v-icon>
+                             Games
+                          </v-btn>
+                        </v-col> 
+
+                      </v-row>
+
+                      <!-- 2a fila de hobbies -->
+                      <v-row> 
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-hiking
+                            </v-icon>
+                             Hiking
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-file-document-edit
+                            </v-icon>
+                              Projects
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-movie-open
+                            </v-icon>
+                             Movies
+                          </v-btn>
+                        </v-col> 
+
+                        <v-col class="pr-0 pl-0"> 
+                          <v-btn color="white" small outlined rounded>
+                            <v-icon left>
+                              mdi-basketball
+                            </v-icon>
+                             Sports
+                          </v-btn>
+                        </v-col> 
+
+                      </v-row>
+
+                      <h3 class="aboutMeTextColor text-left pt-3"> Languages </h3>
+                      <v-divider> </v-divider> 
+
+                      <v-row class="pt-3"> 
+
+                        <v-col>
+                          <v-progress-circular
+                            :rotate="90"
+                            :size="100"
+                            :width="15"
+                            :value="valuePt"
+                            color="blue"
+                          >
+                            {{ valuePt }}
+                          </v-progress-circular>
+                          <p class="aboutMeTextColor pt-2"> Fluent Portuguese </p>
+                        </v-col>
+
+                        <v-col>
+                          <v-progress-circular
+                            :rotate="90"
+                            :size="100"
+                            :width="15"
+                            :value="valueEn"
+                            color="red"
+                          >
+                            {{ valueEn }}
+                          </v-progress-circular>
+                          <p class="aboutMeTextColor pt-2"> Advanced English (CAE) </p>
+                        </v-col>
+
+                        <v-col>
+                          <v-progress-circular
+                            :rotate="90"
+                            :size="100"
+                            :width="15"
+                            :value="valueSp"
+                            color="yellow"
+                          >
+                            {{ valueSp }}
+                          </v-progress-circular>
+                          <p class="aboutMeTextColor pt-2"> Spanish </p>
+                        </v-col>
+
+                      </v-row>
+
+                   
                     </v-col>
 
-                    <v-col cols="8" class="aboutMesecondColColor">
+                    <v-col cols="12" class="aboutMesecondColColor" md="8">
                       merda
                     </v-col>
 
@@ -106,7 +370,45 @@ export default {
   name: 'Home',
   components : { 
     Navbar
-  }
+  }, 
+  data () {
+      return {
+        intervalPt: {},
+        intervalEn : {},
+        intervalSp : {},
+        valuePt: 0,
+        valueEn : 0, 
+        valueSp : 0
+      }
+    },
+    beforeDestroy () {
+      clearInterval(this.intervalPt)
+      clearInterval(this.intervalEn)
+      clearInterval(this.intervalSp)
+    },
+    mounted () {
+      this.intervalPt = setInterval(() => {
+        if (this.valuePt === 100) {
+          return (this.valuePt = 100)
+        }
+        this.valuePt += 10
+      }, 800)
+
+      this.intervalEn = setInterval(() => {
+        if (this.valueEn === 90) {
+          return (this.valueEn = 90)
+        }
+        this.valueEn += 10
+      }, 800)
+
+      this.intervalSp = setInterval(() => {
+        if (this.valueSp === 30) {
+          return (this.valueSp = 30)
+        }
+        this.valueSp += 10
+      }, 800)
+    
+    },
 
 }
 </script> 
@@ -120,6 +422,14 @@ export default {
 
 .aboutMesecondColColor { 
   background-color: #d8d8d8;
+} 
+
+.aboutMeTextColor { 
+  color : white
+}
+
+.theme--light.v-divider {
+    border-color: rgb(255, 252, 252) !important; 
 }
 
 </style>
