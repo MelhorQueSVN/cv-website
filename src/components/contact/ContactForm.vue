874b34d440d14a5133eb54150e7ff327d908d22d
MelhorQueSVN <joaonunes.22.12@gmail.com>
2021-09-28 15:58:08 +0100

    updated styles css with contactform background color

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -2,22 +2,22 @@
     <v-form v-model="allFieldsValid">
         <v-container >
             <v-row justify="center">
-                <v-col cols="12" md="4" class="backColor">
+                <v-col cols="12" md="4" class="contactBackColor">
                     <v-text-field label="Name" v-model="name" clearable :rules="rules_name"> </v-text-field>
                 </v-col>
             </v-row>
             <v-row justify="center" class="pt-0">
-                <v-col cols="12" md="4" class="backColor">
+                <v-col cols="12" md="4" class="contactBackColor">
                     <v-text-field label="Email" v-model="email" clearable :rules="rules_email" > </v-text-field>
                 </v-col>
             </v-row>
             <v-row justify="center" class="pt-0">
-                <v-col cols="12" md="4" class="backColor">
+                <v-col cols="12" md="4" class="contactBackColor">
                     <v-textarea clearable rows="4" clear-icon="mdi-close-circle" label="Text" v-model="message" :rules="rules_message"></v-textarea>
                 </v-col>
             </v-row>
             <v-row justify="center" class="pt-0">
-                <v-col class="backColor" cols="12" md="4">
+                <v-col class="contactBackColor" cols="12" md="4">
                     <v-btn :disabled="!allFieldsValid" text @click="sendEmail()">Submit</v-btn>
                 </v-col>
             </v-row>
@@ -88,10 +88,5 @@ export default {
 }
 </script>
 
-<style scoped>
-
-.backColor { 
-  background-color: #d8d8d8;
-} 
-
+<style scoped src="@/assets/styles/styles.css">
 </style>
