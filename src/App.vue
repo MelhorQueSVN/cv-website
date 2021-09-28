a90a7f170c7b962a2e7f55bad2ebcb0a55910681
MelhorQueSVN <joaonunes.22.12@gmail.com>
2021-09-28 16:05:48 +0100

    loading background in assets

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 
 <style>
 #app {
-  background: url('https://user-images.githubusercontent.com/36746608/134700795-8d8fc46e-2785-49aa-95d7-fa4f8d9a97f9.jpg')
+  background: url('assets/pictures/back.jpg')
     no-repeat center center fixed !important;
   background-size: cover;
 }
